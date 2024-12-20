da4f2f5fd0e791b7cba87e26d54784a3616e78d8
Ganesh Pataiya (UI Developer ) <38519608+ganeshpataiya@users.noreply.github.com>
2024-12-20 14:18:22 +0530

    added latest
    new base for portfolio

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Hello, Ganesh Pataiya !
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://ganeshpataiya.github.io/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          for more.....!
         </a>
       </header>
     </div>
